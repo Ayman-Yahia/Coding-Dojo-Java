3b55c45979b8f0e09be59ba638bd1a39a9beca38
Ayman Yahia <ayman.ya1997@gmail.com>
2021-06-08 20:53:46 +0300

    FizzBuzz

---
+++ java_Fundamentals/FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,7 @@
+public class FizzBuzzTest {
+    public static void main(String[] args){
+        FizzBuzz number = new FizzBuzz();
+        String output = number.fizzBuzz(35);
+        System.out.println("FizzBuzz result = "+output);
+    }
+}
