5a4e90cf3ae3c1446b25e2d8505153d2c52397cb
Ayman Yahia <ayman.ya1997@gmail.com>
2021-06-15 13:52:46 +0300

    update

--- Java Spring/SpringFundamentals/HelloHuman/src/main/java/com/codingdojo/HelloHuman/HomeControler.java
+++ Java Spring/SpringFundamentals/HelloHuman/src/main/java/com/codingdojo/HelloHuman/HomeControler.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/your_server")
 public class HomeControler {
 	@RequestMapping("/")
-	public String hello(@RequestParam(value="first_name", required=false) String searchQuery) {
+	public String hello(@RequestParam(value="first_name", required=false) String searchQuery,@RequestParam(value="last_name", required=false) String search) {
 		if (searchQuery=="") {
 			return "<h1>Hello Human</h1>  <p>Welcome to Spring Boot!</p>";
 						
 		}else {
-			return "Welcome "+searchQuery+"<h6>Welcome to Spring boot!</h6>";
+			return "Welcome "+searchQuery+" "+search+"<h6>Welcome to Spring boot!</h6>";
 		}
 		
 	}
